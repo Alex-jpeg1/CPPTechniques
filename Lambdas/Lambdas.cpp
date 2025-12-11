ac6e018f574f663ab039cc545e7b29a11369afaa
Spita Alexandru <spita.alexandrum@gmail.com>
2025-12-11 02:07:28 +0200

    Going a bit into functors

--- Lambdas/Lambdas.cpp
+++ Lambdas/Lambdas.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
 
+//lambda to add 2 numbers that are read from the keyboard
+//the return type will be void
+class Lambda
+{
+   public:
+      Lambda(int _x, int _y): x(_x), y(_y){}
+
+      inline void operator()() const 
+         {
+            std::cout<<x+y;
+         }
+   private:
+      const int x;
+      const int y;
+};
+//This is called a functor and is a c++ only compatible 
+
+struct LambdaPointer{
+   public:
+      LambdaPointer(){};
+};
+
+//TO DO implement a lambda with a pointer function
 int main()
 {
-   auto Lambda = [](){}; // a simple lambda to see the syntax
+   //auto Lambda = [](){}; // a simple lambda to see the syntax
    
    int x = 10;
    //Now i will create a Lambda object that takes all the variable around and prints their values
@@ -20,7 +43,7 @@ int main()
                         }; //This lambda allows to modify the values of the variables captured though it does not modify the value from the function it captured it from
    Lambda2();
 
-   std::cout<<x<<'\n';
+   //std::cout<<x<<'\n';
 
    auto Lambda3 = [&](){
                          x--;
@@ -28,11 +51,16 @@ int main()
                         }; //This Lambda captures by reference and the values will be modified even outside
    Lambda3();
    
-   std::cout<<x<<'\n'; // x has been modified 
+   //std::cout<<x<<'\n'; // x has been modified 
    
    //Lambdas will be transformed into a class each lambda with its own class 
    //These clases are called functors and are pretty much overloads of the () operator 
    //I will demonstrate this in a code although i will not ensure a 1 to 1 class definition of the Lambdas that are created by the compiler
    
    //TO DO Learn to implement a lambda by hand and try to call it
+   //Done implemented as a functor
+   int y=20;
+   Lambda f = Lambda(x,y);
+   f();
 }
+
