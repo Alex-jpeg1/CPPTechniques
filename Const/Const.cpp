2a7a5bf2567f3e17c5fe94d8956fbe7c786aeef0
Spita Alexandru <spita.alexandrum@gmail.com>
2026-01-03 21:07:03 +0200

    Const member functions

--- Const/Const.cpp
+++ Const/Const.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <vector>
 
@@ -14,7 +15,7 @@ ostream& operator<<(ostream& out, vector<T> v)
     out<<'\n';
     return out;
 }
-
+/* Testing const iterators
 int main()
 {
     //Lets test the const keyword using a vector iterator
@@ -33,15 +34,15 @@ int main()
     const vector<int>::iterator citr = v.begin();//This is a constant iterator that points to non const value
 
     *citr = 40; //This transform the first element to 40
-    //*(++citr) = 50; // This would result in an error as we try to modify the actual value of the pointer not the value it points to
+    // *(++citr) = 50; // This would result in an error as we try to modify the actual value of the pointer not the value it points to
 
     cout<<v;
     //untill now an error occurs as we cannot modify the citr 
 
     vector<int>::const_iterator cvitr = v.begin(); //This iterator can be modified but it cannot modify the values it points to
 
-    //*cvitr = 100; //this line creates an error as we try to modify a const value 
-    //*(++cvitr) = 110; //This line also creates an error for similar reason as the above example although the operator++ allows to modify the value of the iterator
+    // *cvitr = 100; //this line creates an error as we try to modify a const value 
+    // *(++cvitr) = 110; //This line also creates an error for similar reason as the above example although the operator++ allows to modify the value of the iterator
 
     cout<<v;
 
@@ -49,9 +50,43 @@ int main()
 
     const vector<int>::const_iterator ccvitr = v.begin(); //this is a const iterator that points to const value
 
-    //*ccvitr = 120; //This results in an error as we cannot modify the value
-    //*(++ccvitr) = 130; //This results first in an error of trying to increment the pointer which is const although it cannot modify the value either
+    // *ccvitr = 120; //This results in an error as we cannot modify the value
+    // *(++ccvitr) = 130; //This results first in an error of trying to increment the pointer which is const although it cannot modify the value either
 
     cout<<v;
     //untill now still 2 errors but we cannot modify the value with *(++ccvitr) the incrementing error simply occurs first 
+}
+*/
+
+namespace constMemberFunctions
+{
+
+    class Test
+    {
+    public:
+
+    Test(): text("ABCDEFG"){}
+
+    char& operator[](std::size_t indices) { std::cout<<"NON CONST CALL\n"; return text[indices]; }
+
+    const char& operator[](std::size_t indices) const {std::cout<<"CONST CALL\n"; return text[indices]; } 
+
+    Test operator*(const Test& rhs) {std::cout<<"nonCONST OPERATOR*\n"; return rhs;}
+    const Test operator*(Test& rhs) const {std::cout<<"CONST OPERATOR*\n"; return rhs;}
+    private:
+    std::string text;
+    };
+    
+
+}
+
+int main()
+{
+    const constMemberFunctions::Test test1;
+    test1[1];
+    constMemberFunctions::Test test2;
+    test2[2];
+
+    test1*test2;
+    test2*test1;
 }
