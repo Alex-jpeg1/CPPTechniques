9951209ab57b56e1b26c89f0963615b603fdad6f
Spita Alexandru <spita.alexandrum@gmail.com>
2026-01-05 03:16:19 +0200

    Some vexing parse examples

--- GoodOOPPractices/GoodOOPPractices.cpp
+++ GoodOOPPractices/GoodOOPPractices.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+
 int main()
 {
-    
+    //int vexingParse(); //This can be interpreted as a function and creates ambiguity
+    //This is mostly seen when attempting to create an object and try to explicitly call its default constructor 
+    class VexingParseExample
+    {
+        public:
+        VexingParseExample() = default;
+    };
+
+    //VexingParseExample _VexingParseExample;//written like this it calls the constructor by default and creates no ambiguity
+    //VexingParseExample _VexingParseExample(); //written like this can be mistaken for a function signature and creates ambiguity
+    //VexingParseExample _VexingParseExample{}; //written like this is a workaround of the explicit default constructor call that raises no error 
 }
